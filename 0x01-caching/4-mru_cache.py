201f33a60aa47b1d50cbf382b19c092c06da6561
mostafazhrn <mostafazhrn@gmail.com>
2024-06-26 16:34:43 -0400

    push_task4_fix

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" THis script shall create class basic_cache that inherits from BaseCaching """
+""" THis script shall create basic_cache that inherits from BaseCaching """
 BaseCaching = __import__('base_caching').BaseCaching
 
 
@@ -15,18 +15,18 @@ class MRUCache(BaseCaching):
         """ This method assign item value to the key in self.cache_data """
         if key is not None and item is not None:
             self.cache_data[key] = item
-        if key in self.keys:
-            self.keys.remove(key)
-        self.keys.append(key)
-        if len(self.keys) > BaseCaching.MAX_ITEMS:
-            del_key = self.keys.pop()
-            del self.cache_data[del_key]
-            print("DISCARD: {:s}".format(del_key))
+            if key not in self.keys:
+                self.keys.append(key)
+            else:
+                self.keys.append(self.keys.pop(self.keys.index(key)))
+            if len(self.keys) > BaseCaching.MAX_ITEMS:
+                del_ky = self.keys.pop(-2)
+                del self.cache_data[del_ky]
+                print("DISCARD: {:s}".format(del_ky))
 
     def get(self, key):
         """ This method returns the value in self.cache_data linked to key """
         if key is not None and key in self.cache_data:
-            self.keys.remove(key)
-            self.keys.append(key)
+            self.keys.append(self.keys.pop(self.keys.index(key)))
             return self.cache_data[key]
         return None
