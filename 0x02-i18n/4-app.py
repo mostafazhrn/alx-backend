80d3c68db66ddc7d98127173bc2e09729a1e45a7
mostafazhrn <mostafazhrn@gmail.com>
2024-07-02 17:52:20 -0400

    push_task4

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+""" THis script starts a Flask web application """
+from flask import Flask, render_template, request
+from flask_babel import Babel, _
+
+
+class Config(object):
+    """ this shall reptesent the config class"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_DEFAULT_LOCALE = 'en'
+
+
+app = Flask(__name__, template_folder='templates')
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """ this function shall return the best match of supported languages """
+    local = request.args.get('locale', None)
+    if local and local in app.config['LANGUAGES']:
+        return local
+    
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index():
+    """ This function returns a string """
+    return render_template('4-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
